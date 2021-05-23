6d4fad087c1ec1e5f51ebc3c85aeb443480b1930
Jae-Woo Joo <wnwodn4293@naver.com>
2021-05-23 22:33:29 +0900

    [feat] 로그인 input창 생성
    - 로그인 페이지 input 창 생성

---
+++ fe/src/components/login/input/index.tsx
@@ -0,0 +1,50 @@
+import React, { FC, useState } from 'react';
+import { gql, useMutation } from '@apollo/client';
+import router from 'next/router';
+import { StyledInput, StyledButton } from './styled';
+
+const LOCAL_LOGIN = gql`
+  mutation ($email: String!, $password: String!) {
+    login(email: $email, password: $password)
+  }
+`;
+
+const Input: FC = () => {
+  const [email, setEmail] = useState('');
+  const [pwd, setPwd] = useState('');
+  const [login, { data }] = useMutation(LOCAL_LOGIN);
+
+  const clickButton = async (id: string, password: string) => {
+    if (!id) alert('이메일을 입력해주세요');
+    else if (!password) alert('패스워드를 입력해주세요');
+    else {
+      await login({ variables: { email: id, password } });
+      if (!data) alert('이메일 또는 비밀번호가 틀립니다!');
+      else router.replace('/');
+    }
+  };
+
+  return (
+    <>
+      <StyledInput
+        type="text"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="이메일"
+      />
+      <br />
+      <StyledInput
+        type="password"
+        value={pwd}
+        onChange={(e) => setPwd(e.target.value)}
+        placeholder="패스워드"
+      />
+      <br />
+      <StyledButton type="button" onClick={() => clickButton(email, pwd)}>
+        확인
+      </StyledButton>
+    </>
+  );
+};
+
+export default Input;
---
+++ fe/src/components/login/input/styled.ts
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+
+export const StyledInput = styled.input`
+  height: 13%;
+  width: 60%;
+`;
+
+export const StyledButton = styled.button`
+  height: 13%;
+  margin: 0.5em;
+  width: 60%;
+`;
