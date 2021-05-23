5838a9aeb1c38360fa032fbf6afbd2b31e587104
Jae-Woo Joo <wnwodn4293@naver.com>
2021-05-23 22:30:23 +0900

    [feat] 로그인 api 구현
    - 유저 모델 생성
    - 로그인 리졸버, 서비스 코드 생성

--- be/src/user/user.resolver.ts
+++ be/src/user/user.resolver.ts
@@ -1,21 +1,40 @@
-import { NotFoundException } from '@nestjs/common';
-import { Args, Mutation, Query, Resolver, Subscription } from '@nestjs/graphql';
-import { PubSub } from 'apollo-server-express';
-import { User } from './models/user.model';
+import { Args, Context, Mutation, Query, Resolver, Directive } from '@nestjs/graphql';
+import { Response } from 'express';
+import { User } from '../models/user.model';
 import { UserService } from './user.service';
 
-const pubSub = new PubSub();
-
-@Resolver((of) => User)
+@Resolver(() => User)
 export class UserResolver {
-  constructor(private readonly userService: UserService) {}
+  constructor(private userService: UserService) {}
+  @Directive('@auth')
+  @Query(() => User)
+  async myInfo(@Context() { authUser }: { authUser: string }) {
+    const info = await this.userService.getMyInfo(authUser);
+    return info;
+  }
+  @Directive('@auth')
+  @Query(() => [User])
+  async friends() {
+    return this.userService.getFriends();
+  }
+  @Mutation(() => User)
+  async createUser(
+    @Args('email') email: string,
+    @Args('nickName') nickName: string,
+    @Args('password') password: string,
+  ) {
+    return this.userService.createUser(email, nickName, password);
+  }
 
-  @Query((returns) => User)
-  async user(@Args('id') id: string): Promise<User> {
-    const user = await this.userService.findOneById(id);
-    if (!user) {
-      throw NotFoundException;
-    }
-    return user;
+  @Mutation(() => Boolean)
+  async login(
+    @Args('email') email: string,
+    @Args('password') password: string,
+    @Context() { res }: { res: Response },
+  ) {
+    const token = await this.userService.login(email, password);
+    if (!token) return false;
+    res.cookie('jwt', token, { httpOnly: true, sameSite: 'strict' });
+    return true;
   }
 }
