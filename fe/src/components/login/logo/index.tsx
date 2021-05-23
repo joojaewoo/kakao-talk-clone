900746ae37f9c9ac134983a7e4463995214df78f
Jae-Woo Joo <wnwodn4293@naver.com>
2021-05-23 22:34:02 +0900

    [feat] 로고 컴포넌트 생성
    - 로고 컴포넌트 생성
    - static 폴더에서 읽어오기 실패.

---
+++ fe/src/components/login/logo/index.tsx
@@ -0,0 +1,6 @@
+import React, { FC } from 'react';
+import Image from './styled';
+
+const Logo: FC = () => <Image src="/images/logo.png" alt="me" />;
+
+export default Logo;
---
+++ fe/src/components/login/logo/styled.ts
@@ -0,0 +1,6 @@
+import styled from '@emotion/styled';
+
+export default styled.img`
+  width: 30%;
+  height: 30%;
+`;
