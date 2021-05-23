a03ed7590c7aca54332590f7d68fb50edecf3bc1
Jae-Woo Joo <wnwodn4293@naver.com>
2021-05-23 22:32:28 +0900

    [style] 전역 스타일 설정
    - 전역 스타일 설정 ( 모바일에 맞춰 )

---
+++ fe/src/theme/index.ts
@@ -0,0 +1,15 @@
+export const theme = {
+  PRIMARY: '#19CE60',
+  BORDER: '#bfbfbf',
+  FONT: '#2c2c2c',
+  LIGHT: '#fff',
+  RED: '#ff4d4f',
+  MODAL_BACKGROUND: 'rgba(90,153,115,0.75)',
+  MODAL_OVERLAY: 'rgba(0,0,0,0.3)',
+  LIGHT_GRAY: '#eaeaea',
+  LIGHT_BLUE: '#add8e6',
+};
+
+export type Theme = typeof theme;
+
+export default theme;
