7e9aa92a4dcdfed53f689c4ef140eb558fddcebc
Jae-Woo Joo <wnwodn4293@naver.com>
2021-05-23 22:36:39 +0900

    [feat] 인증 프로바이더 구현
    - 로그인된 유저인지 확인하는 인증 프로바이더 구현

---
+++ fe/src/libs/authProvider.tsx
@@ -0,0 +1,31 @@
+import React, { FunctionComponent, ReactChild } from 'react';
+import { useQuery, gql } from '@apollo/client';
+import { useRouter } from 'next/router';
+
+interface Props {
+  children: ReactChild;
+}
+
+const AuthProvider: FunctionComponent<Props> = ({ children }) => {
+  const router = useRouter();
+  const { data, error } = useQuery(
+    gql`
+      query {
+        myInfo {
+          _id
+          email
+          nickName
+        }
+      }
+    `,
+  );
+
+  if (router.pathname.includes('login') || router.pathname.includes('callback'))
+    return <>{children}</>;
+
+  if (data) return <>{children}</>;
+  if (error) router.push('/login');
+  return <div>loading</div>;
+};
+
+export default AuthProvider;
