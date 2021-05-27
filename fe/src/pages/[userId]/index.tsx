1fb2f8ac2694040c675c4d62b706b89eef979f89
Jae-Woo Joo <wnwodn4293@naver.com>
2021-05-27 16:17:41 +0900

    [feat] 프로필 페이지 구현
    - serversideProps 설정

---
+++ fe/src/pages/[userId]/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { GetServerSideProps } from 'next';
+import { USER_INFO } from '../../graphql/user';
+import getApolloClient from '../../libs/apolloClient';
+import ProfilePage from '../../components/Profile';
+
+const Profile = () => <ProfilePage />;
+
+export default Profile;
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const jwt = '';
+  const { userId } = ctx.query || {};
+  const apolloClient = getApolloClient();
+  const { data } = await apolloClient.query({
+    query: USER_INFO,
+    variables: { userId },
+    context: {
+      headers: { cookie: `jwt=${jwt}` },
+    },
+  });
+  if (!data.user) {
+    return {
+      notFound: true,
+    };
+  }
+
+  const initialState = apolloClient.cache.extract();
+  return {
+    props: { initialState },
+  };
+};
