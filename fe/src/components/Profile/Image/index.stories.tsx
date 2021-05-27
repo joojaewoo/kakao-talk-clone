8c140c9ead9cff97bc585ede745ebea5b929318d
Jae-Woo Joo <wnwodn4293@naver.com>
2021-05-27 16:17:04 +0900

    [feat] 이미지 스토리북 구현
    - 프로필 이미지 컴포넌트 스토리북 구현

---
+++ fe/src/components/Profile/Image/index.stories.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Image from './index';
+
+export default {
+  title: 'Profile/Image',
+  component: Image,
+};
+
+export const Default = () => (
+  <Image
+    name="주재우"
+    imageUrl="https://user-images.githubusercontent.com/46195613/119775073-0f253780-befe-11eb-971f-d388c738a545.jpg"
+  />
+);
