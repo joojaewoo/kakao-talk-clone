f3c4a819809a0cb05eb51fc7e02b0bcd59f6a2af
Jae-Woo Joo <wnwodn4293@naver.com>
2021-05-31 02:24:58 +0900

    [refactor] 반환값 수정
    - 필요없는 변수 선언 제거

--- be/src/libs/utils.ts
+++ be/src/libs/utils.ts
@@ -1,6 +1,4 @@
-export const getRandomNum = () => {
-  const num = Math.floor(Math.random() * 1000000)
+export const getRandomNum = () =>
+  Math.floor(Math.random() * 1000000)
     .toString()
     .padStart(6, '0');
-  return num;
-};
